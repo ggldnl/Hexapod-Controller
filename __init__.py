9e78db8837a62273995a954103e093b407e0c7c2
Re-l <danielgigliotti99.dg@gmail.com>
2026-02-14 21:34:47 +0100

    Packaged

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""Hexapod Controller Package"""
+
+from controller.model.controller import HexapodController
+
+# What gets imported with "from controller import *"
+__all__ = ['HexapodController']
+
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hexapod",
+    version="0.1.0",
+    author="ggldnl",
+    author_email="danielgigliotti99.dg@gmail.com",
+    description="Hexapod controller package",
+    long_description_content_type="text/markdown",
+    url="https://github.com/ggldnl/Hexapod-Controller.git",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "pyserial",
+        "sshkeyboard",
+    ],
+)
