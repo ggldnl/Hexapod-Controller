c3bda2218d6a8866089e0e262dc8876024491c93
Re-l <danielgigliotti99.dg@gmail.com>
2026-03-21 12:04:55 +0100

    Exposed Kernel and Interface

--- controller/__init__.py
+++ controller/__init__.py
@@ -2,13 +2,18 @@
 
 from .model.controller import HexapodController
 from .model.controller import State as HexapodState
+from .model.interface import Interface as HexapodInterface
+from .hardware.kernel import Kernel as HexapodKernel
 
 from .model.gaits import *
 from .hardware.HDLC import *
-from .hardware.kernel import *
-from .utils.calibration import *
 
 # What gets imported with "from controller import *"
-__all__ = ['HexapodController', 'HexapodState']
+__all__ = [
+    'HexapodController',
+    'HexapodState',
+    'HexapodKernel',
+    'HexapodInterface'
+]
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
